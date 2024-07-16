bd9952a5d91006800abba7025a469249cc2098d4
unknown <you@example.com>
2024-07-16 18:05:33 +0530

    Some of the codes

---
+++ Sai.py
@@ -0,0 +1,11 @@
+public class sai{
+    public static void main(String[],args)
+    int a =30;
+    if a>=18:{
+    System .out.println("He is  eligible for the vote")
+
+    }
+    else{
+        System.out.println("He is not eligible for vote")
+    }
+}
