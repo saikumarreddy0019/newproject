5cc40869ae82e69b77bbc8b53df69c3af6e91c23
unknown <you@example.com>
2024-07-16 19:13:22 +0530

    New changes are made

--- sai.java
+++ sai.java
@@ -1,11 +1,11 @@
 public class sai{
     public static void main(String[],args)
     int a =30;
-    if a>=18:{
-    System .out.println("He is  eligible for the vote")
+    if a>=60:{
+    System .out.println("He is  eligible for the pension")
 
     }
     else{
-        System.out.println("He is not eligible for vote")
+        System.out.println("He is not eligible for pension")
     }
 }
